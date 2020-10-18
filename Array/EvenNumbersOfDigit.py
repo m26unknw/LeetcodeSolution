cb79561729128fc14d498da887125770fa5f3a62
m26unknw <shukla9915294376@gmail.com>
2020-10-18 15:40:10 +0530

    Added basic princhiple

--- Array/EvenNumbersOfDigit.py
+++ Array/EvenNumbersOfDigit.py
@@ -1,4 +1,7 @@
 #Find Numbers with Even Number of Digits
+#In this solution I i did not count the digit of numbers by doing the divide remainder method i just convert the number into string
+# And then just checking with length whether it is even or not:
+
 
 class solution:
     def findNumbers(self,nums):
@@ -10,4 +13,19 @@ class solution:
 
         return counter
 
+#Second Method
+
+class solution_2:
+    def findNumbers(self,nums):
+        result=0
+        for eachRound in range(len(nums)):
+            counter=0
+            while eachRound:
+                eachRound//10
+                counter+=1
+            
+            if eachRound%2==0:
+                result+=1
+
+        return result
 
